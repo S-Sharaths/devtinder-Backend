9411bcc892638c5c10575df898b93d995ea9c219
S-Sharaths <sharathprogramming4@gmail.com>
2025-03-05 11:14:47 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+// means we are creating the express js application
+// i
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("this is main !");
+});
+
+// below function is know as request handler
+app.use("/test", (req, res) => {
+  res.send("Hello from the server !");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello hello !");
+});
+
+// we will have to listen to incoming request
+app.listen(1234, () => {
+  console.log("the server is sucessfully listen  on port 1234");
+});

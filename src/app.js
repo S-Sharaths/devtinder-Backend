84625d2bd26b9bfa4e43f5547472a290b73d7f6d
S-Sharaths <sharathprogramming4@gmail.com>
2025-03-06 13:08:09 +0530

    after using two route handler in a route

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const app = express();
 
-// this will match all the HTTP method API calls to /test
-//it will the request like /user  ,/user/xyz , /user/1
-app.get("/user/:userid/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send("Hello from the server !");
-});
+app.use(
+  "/user",
 
-// we will have to listen to incoming request
+  (req, res, next) => {
+    console.log("handling the route user 2");
+    // res.send("reponse 1 !!");
+
+    next();
+  },
+
+  (req, res) => {
+    console.log("handling the route user 2");
+    res.send("reponse 2 !!");
+  }
+);
+
 app.listen(1234, () => {
   console.log("the server is sucessfully listen  on port 1234");
 });

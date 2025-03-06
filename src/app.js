17911edf2d6dcf6f8ecb2e0e0455d922a98f4428
S-Sharaths <sharathprogramming4@gmail.com>
2025-03-06 11:26:54 +0530

    after using dynamic routes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,11 @@
 const express = require("express");
 
-// means we are creating the express js application
-// i
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("HAHAHA");
-});
-
-// this wiil only handle GET call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "sharath", lastname: "s" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("data sucessfuly saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("data sucessfuly delete from database");
-});
-
 // this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
+//it will the request like /user  ,/user/xyz , /user/1
+app.get("/user/:userid/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send("Hello from the server !");
 });
 

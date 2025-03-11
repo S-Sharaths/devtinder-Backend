b3a820531d9ac04f9b9ac0b654748334e64cc052
S-Sharaths <sharathprogramming4@gmail.com>
2025-03-11 14:23:32 +0530

    after independent route

--- src/app.js
+++ src/app.js
@@ -2,33 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
+app.get("/user", (req, res, next) => {
+  console.log("handling the route user 2");
+  res.send("reponse 2 !!");
+});
 
-  (req, res, next) => {
-    console.log("handling the route user 2");
-    next();
-    // res.send("reponse 1 !!");
-  },
-
-  (req, res, next) => {
-    console.log("handling the route user 2");
-    // res.send("reponse 2 !!");
-    next();
-  },
-
-  (req, res, next) => {
-    console.log("handling the route user 2");
-    // res.send("reponse 2 !!");
-    next();
-  },
-
-  (req, res, next) => {
-    console.log("handling the route user 2");
-    // res.send("reponse 2 !!");
-    // next();
-  }
-);
+app.get("/user", (req, res, next) => {
+  console.log("handling the route user !!");
+  next();
+  // res.send("reponse 1 !!");
+});
 
 app.listen(1234, () => {
   console.log("the server is sucessfully listen  on port 1234");

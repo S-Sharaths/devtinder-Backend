23936d49f3f360acbb25abd5bb57dcac3527eaf7
S-Sharaths <sharathprogramming4@gmail.com>
2025-03-12 18:24:43 +0530

    after calling signup api and error handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database");
-
 const app = express();
+const User = require("./models/user");
+
+app.post("/signUp", async (req, res) => {
+  const userObject = {
+    firstName: "sharath",
+    lastName: "S",
+    emailID: "ss@gmail.com",
+    password: "ss@00",
+  };
+
+  // we are creating a new instance of the user modelS
+  const user = new User(userObject);
+
+  try {
+    await user.save();
+    res.send("user added sucessfully");
+  } catch (err) {
+    res.status(400).send("error saving the user" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {

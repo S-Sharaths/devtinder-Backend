4c5840d058c9874c6110d0b69283a5ceda00b6e4
S-Sharaths <sharathprogramming4@gmail.com>
2025-03-13 15:51:26 +0530

    after creating get apis

--- src/app.js
+++ src/app.js
@@ -17,6 +17,61 @@ app.post("/signUp", async (req, res) => {
   }
 });
 
+// get user by email
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailID;
+
+  //Get user By email
+  try {
+    const user = await User.find({
+      emailID: userEmail,
+    });
+
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//Get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+
+    if (user.length === 0) {
+      res.status(404).send("no users found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/Oneuser", async (req, res) => {
+  const userEmail = req.body.emailID;
+
+  //Get user By email
+  try {
+    const user = await User.findOne({
+      emailID: userEmail,
+    });
+
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection was successful");
